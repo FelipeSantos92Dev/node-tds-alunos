26301c2168afd5dab9524ff32ef29a5a52b8b17f
Felipe Santos <dev.felipesantos@gmail.com>
2023-11-14 19:34:43 -0300

    feat: create students routes

---
+++ src/routes/students.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+
+import {
+  getStudents,
+  getStudent,
+  createStudent,
+  updateStudent,
+  deleteStudent,
+} from "../controllers/students.controller.js";
+
+const studentsRouter = Router();
+
+studentsRouter.get("/", getStudents);
+studentsRouter.get("/:id", getStudent);
+studentsRouter.post("/", createStudent);
+studentsRouter.put("/:id", updateStudent);
+studentsRouter.delete("/:id", deleteStudent);
+
+export { studentsRouter };
